2c23107544202f0b068904e36ce73d9ec4e8de30
miqdad71 <miqdad.ihbs71777@gmail.com>
2021-04-04 14:32:04 +0700

    Get data engineer by localHost and bring access to http

--- lib/src/ui/main/mainpage/home/home.dart
+++ lib/src/ui/main/mainpage/home/home.dart
@@ -1,14 +1,14 @@
-import 'dart:convert';
-
 import 'package:flutter/material.dart';
+import 'dart:convert';
 import 'package:http/http.dart' as http;
-import 'package:starworks_hireapp_flutter/constants.dart';
+
+
+
 
 class Home extends StatefulWidget {
   @override
   _HomeState createState() => _HomeState();
 }
-
 class _HomeState extends State<Home> {
   List users = [];
   bool isLoading = false;
@@ -19,15 +19,16 @@ class _HomeState extends State<Home> {
     super.initState();
     this.fetchUser();
   }
+
   fetchUser() async {
     setState(() {
       isLoading = true;
     });
-    var url = "https://randomuser.me/api/?results=50";
-    // var url = "http://10.0.2.2:4000/engineer";
+    // var url = "https://randomuser.me/api/?results=50";
+    var url = "http://10.0.2.2:4000/engineer";
     var response = await http.get(url);
     if(response.statusCode == 200){
-      var items = json.decode(response.body)['results'];
+      var items = json.decode(response.body)['data'];
       setState(() {
         users = items;
         isLoading = false;
@@ -52,9 +53,6 @@ class _HomeState extends State<Home> {
   }
 
   Widget getBody() {
-    if(users.contains(null) || users.length < 0 || isLoading){
-      return Center(child: CircularProgressIndicator(valueColor: new AlwaysStoppedAnimation(primaryColor),));
-    }
     return ListView.builder(
         itemCount: users.length,
         itemBuilder: (context, index) {
@@ -63,9 +61,11 @@ class _HomeState extends State<Home> {
   }
 
   Widget getCard(item) {
-    var fullName = item['name']['title']+""+item['name']['first']+""+item['name']['last'];
-    var email = item['email'];
-    var profileUrl = item['picture']['large'];
+    var fullName = item['ac_name'];
+    var email = item['en_job_title'];
+    var profileUrl = item['en_profile'];
+    var location = item['en_domicile'];
+
     return Card(
       child: Padding(
         padding: const EdgeInsets.all(10.0),
@@ -80,7 +80,7 @@ class _HomeState extends State<Home> {
                     borderRadius: BorderRadius.circular(60 / 2),
                     image: DecorationImage(
                         fit: BoxFit.cover,
-                        image: NetworkImage(
+                        image: NetworkImage('http://10.0.2.2:4000/images/'+
                             profileUrl))),
               ),
               SizedBox(
@@ -112,7 +112,7 @@ class _HomeState extends State<Home> {
                         color: Colors.grey,
                       ),
                       Text(
-                        "Bekasi",
+                        location,
                         style: TextStyle(color: Colors.grey, fontSize: 12),
                       )
                     ],
@@ -125,11 +125,4 @@ class _HomeState extends State<Home> {
       ),
     );
   }
-
-/*@override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: Body(),
-    );
-  }*/
 }
